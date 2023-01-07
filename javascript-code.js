35606a36b415927dbf4ecebcdecc072b0fba7a0f
developermoon59 <maxi-rhp@hotmail.com>
2023-01-07 19:18:49 -0300

    all files are added for the first time

---
+++ javascript-code.js
@@ -0,0 +1,20 @@
+
+    function letterReplacement() {
+        var getEncrypted = document.getElementById("getEncrypted").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
+        var encryptedResult = getEncrypted;
+        var textToReplace = encryptedResult;
+        var replacementResult = encryptedResult.replaceAll("a","ai").replaceAll("e","enter").replaceAll("i","imes").replaceAll("o","ober").replaceAll("u","ufat");
+        document.getElementById("visualizeEncrypted").innerHTML = replacementResult;
+    }
+    function invertedReplacementLetter() {
+        var getEncrypted = document.getElementById("getEncrypted").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
+        var encryptedResult = getEncrypted;
+        var textToReplace = encryptedResult;
+        var replacementResult = encryptedResult.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u");
+        document.getElementById("visualizeEncrypted").innerHTML = replacementResult;
+    }
+    function copyText() {
+        var copyField = document.getElementById("visualizeEncrypted");  
+        copyField.select();
+        document.execCommand("copy")
+    }

8e5c83a4192802618953687e4cd588c680481593
developermoon59 <maxi-rhp@hotmail.com>
2023-01-08 15:17:28 -0300

    general update of all code

--- javascript-code.js
+++ javascript-code.js
@@ -1,15 +1,13 @@
 
     function letterReplacement() {
         var getEncrypted = document.getElementById("getEncrypted").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
-        var encryptedResult = getEncrypted;
-        var textToReplace = encryptedResult;
+        var encryptedResult = getEncrypted; 
         var replacementResult = encryptedResult.replaceAll("a","ai").replaceAll("e","enter").replaceAll("i","imes").replaceAll("o","ober").replaceAll("u","ufat");
         document.getElementById("visualizeEncrypted").innerHTML = replacementResult;
     }
     function invertedReplacementLetter() {
         var getEncrypted = document.getElementById("getEncrypted").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
         var encryptedResult = getEncrypted;
-        var textToReplace = encryptedResult;
         var replacementResult = encryptedResult.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u");
         document.getElementById("visualizeEncrypted").innerHTML = replacementResult;
     }
